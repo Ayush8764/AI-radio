f2a28027a5575ea7e2e72d141e3a77e2490dffa2
Ayush kumar <75039947+Ayush8764@users.noreply.github.com>
2022-06-07 08:58:33 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:trixi/pages/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+
+  runApp(MyApp());
+}
+
+// ignore: use_key_in_widget_constructors
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(fontFamily: GoogleFonts.poppins().fontFamily),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
